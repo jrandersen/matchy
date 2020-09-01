12fe092a62ee457718301759d965e88fcd6da09b
jrandersen-ga <andersen.ja@gmail.com>
2020-08-31 21:56:28 -0400

    added global params to css file for colors and text, worked out the flipping css, and added js listner

--- js/app.js
+++ js/app.js
@@ -2,3 +2,16 @@
  * First game
  */
 console.log('==== GAME START ====')
+
+
+
+
+
+/**
+ * CARD FLIP LISTNER
+ */
+const card__one = document.getElementsByClassName('card__one')[0];
+console.log(card__one);
+card__one.addEventListener('click', function() {
+    card__one.classList.toggle('flip');
+});
