96785c8a614272109d5cfbbc014f3e4b646a89a6
jrandersen-ga <andersen.ja@gmail.com>
2020-08-31 23:04:28 -0400

    duplicated card, styled score and timer

--- js/app.js
+++ js/app.js
@@ -15,3 +15,9 @@ console.log(card__one);
 card__one.addEventListener('click', function() {
     card__one.classList.toggle('flip');
 });
+
+const card__two = document.getElementsByClassName('card__two')[0];
+console.log(card__one);
+card__two.addEventListener('click', function() {
+    card__two.classList.toggle('flip');
+});
